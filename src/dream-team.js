e38d4a19db23271b4931c2a1ac06c81d6b4af678
AlexStepanchuk <666stepashka666@gmail.com>
2021-02-02 22:08:49 +0300

    progress: 3/10

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false
+  let result = []
+  members.forEach(element => {
+    if (typeof element === 'string') {
+      result.push(element.trim()[0])
+    }
+  });
+  return result.join("").toUpperCase().split("").sort().join("")
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  //not Array
+  if (!Array.isArray(arr)) throw new Error ('Error');
+  //empty Array
+  if (arr.length == 0) return [];
+  //if haven`t controls
+  let result = [];
+  result  = arr.filter(value => value == '--discard-next' || value == '--discard-prev' ||
+  value == '--double-prev' || value == '--double-next');
+  if (result.length == 0) return arr;
+  //main variant
+  const resultArr = arr.reduce((temp, num, i) => {
+    if (arr[i - 1] === '--discard-next') return temp
+    if (arr[i - 1] === '--double-next') temp.push(arr[i])
+    if (arr[i + 1] === '--double-prev') temp.push(arr[i])
+    if (arr[i + 1] === '--discard-prev') return temp
+    if (num === '--discard-next'
+      || num === '--double-prev'
+      || num === '--double-next'
+      || num === '--discard-prev') {
+      return temp
+    }
+    temp.push(arr[i])
+    return temp
+  }, [])
+  return resultArr
 };
